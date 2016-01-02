5e1959fe60c46efd194905a48380ac12c5fdf607
Security Lab Project Account <seclab@seclab.soic.indiana.edu>
2016-01-02 09:46:44 -0500

    fix

--- bph/whoisData/generateKwList.sh
+++ bph/whoisData/generateKwList.sh
@@ -7,63 +7,67 @@ fi
 sourceDir=$1
 resultDir=$2
 date=$(date +%Y%m%d)
+#cmd="grep -E -i  '^$kw:' $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist"
 mkdir -p $resultDir/$date
+mkdir -p $resultDir/lastest
 gzip -d $sourceDir/$date/*.gz
 
 type="inet6num"
 kw="inet6num"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 
 type="role"
 kw="nic-hdl"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="organisation"
 kw="organisation"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="irt"
 kw="irt"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="mntner"
 kw="mntner"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="aut-num"
 kw="aut-num"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 
 type="as-set"
 kw="as-set"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="as-block"
 kw="as-block"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 
 type="route-set"
 kw="route-set"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="domain"
 kw="domain"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
 
 type="inetnum"
 kw="inetnum"
 echo $type $kw
-grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r 's/$kw:[ ]+(.*)/\1/g' > $resultDir/$date/${type}_kwlist
+grep -E -i  "^$kw:" $sourceDir/$date/ripe.db.$type | sed -r "s/$kw:[ ]+(.*)/\1/g" > $resultDir/$date/${type}_kwlist
+
+cp -r $resultDir/$date/* $resultDir/lastest
